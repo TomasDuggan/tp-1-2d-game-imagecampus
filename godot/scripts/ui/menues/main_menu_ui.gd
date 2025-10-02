105f2b2f4e9b3d9a96eac23a6397c06ad2ded51f
Tomas Duggan <tomyduggan@gmail.com>
2025-10-02 19:56:23 -0300

    feature: credits menu

--- godot/scripts/ui/menues/main_menu_ui.gd
+++ godot/scripts/ui/menues/main_menu_ui.gd
@@ -1,10 +1,14 @@
 extends Control
 class_name MainMenuUI
 
-
+@export_category("Config")
 @export var _music: AudioStream
 
+@export_category("Editor Dependencies")
+@export var _credits_container: Control
+
 func _ready():
+	_credits_container.hide()
 	AudioEventBus.raise_event_play_music(_music)
 
 func _on_play_button_pressed():
@@ -12,3 +16,9 @@ func _on_play_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_credits_button_pressed():
+	_credits_container.show()
+
+func _on_close_credits_button_pressed():
+	_credits_container.hide()
